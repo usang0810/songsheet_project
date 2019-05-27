b6c6bdba39f05038b78e7cfdeddee7eb320c6d37
usang0810 <jysrmb@gmail.com>
2019-05-28 05:17:01 +0900

    Co-authored-by: j0n9m1n1 <j0n9m1n1@gmail.com>

--- total_process.py
+++ total_process.py
@@ -82,35 +82,60 @@ def first_labeling(src):
         if area >3500: # label의 넓이가 3500 이상일때만 사각형
             label_ary.append(stats[i])
             cv2.rectangle(dst, (x, y), (x+width, y+height), 255, -1)
+
+            
         else:
             cv2.rectangle(dst, (x, y), (x+width, y+height), 0, -1)
 
     return dst, label_ary
 
 # 2차 레이블링 - 1차 레이블링을 통해 나눈 영역들을에서 요소들을 레이블링
-def second_labeling(label_ary, ary_inv):
+# def second_labeling(label_ary, ary_inv):
 
-    for i in range(len(label_ary)):
-        cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(ary_inv[i])
+#     for i in range(len(label_ary)):
+#         cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(ary_inv[i])
 
-        for j in range(1, int(cnt)):
-            x, y, width, height, area = stats[j] # stats는 1부터 시작
-            cv2.rectangle(label_ary[i], (x, y), (x+width, y+height), 0, 1)
+#         for j in range(1, int(cnt)):
+#             x, y, width, height, area = stats[j] # stats는 1부터 시작
+#             cv2.rectangle(label_ary[i], (x, y), (x+width, y+height), 0, 1)
         
-        cv2.imshow("roi"+str(i), label_ary[i])
+#         cv2.imshow("roi"+str(i), label_ary[i])
 
 def second_labeling(src):
     cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(src)
-    # dst = np.zeros(src.shape, dtype = src.dtype)
+    dst = np.zeros(src.shape, dtype = src.dtype)
     label_ary = []
     for i in range(1, int(cnt)):
         
         x, y, width, height, area = stats[i] # stats는 1부터 시작 0은 이미지 전체영역
+        # print(area)
+        # label_ary.append(stats[i])
+        if 100 <= area <= 200: # label의 넓이가 3500 이상일때만 사각형
+            # print("src:",src[y:y+height, x:x+width])
+            src_sum = cv2.mean(src[y:y+height, x:x+width]) # 이미지의 평균값이 150이하이면 label_ary에 추가
+            # print(src_sum)
+            if src_sum[0] < 150:
+                label_ary.append(stats[i])
+                cv2.rectangle(dst, (x, y), (x+width, y+height), 255, -1)
 
-        label_ary.append(stats[i])
-        cv2.rectangle(src, (x, y), (x+width, y+height), 255, 1)
+        else:
+            cv2.rectangle(dst, (x, y), (x+width, y+height), 0, -1)
+            pass
+        # cv2.rectangle(src, (x, y), (x+width, y+height), 255, 1)
 
-    return src, label_ary
+    return dst, label_ary
+
+def third_labeling(src):
+    cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(src)
+    label_ary = []
+    print(cnt)
+    for i in range(1, int(cnt)):
+        x, y, width, height, area = stats[i] # stats는 1부터 시작 0은 이미지 전체영역
+        label_ary.append([int((x*2+width)/2), int((y*2+height)/2)])
+        
+        print("label_array: ", label_ary[i-1])
+        
+    return label_ary
 
 def make_roi(src, ary):
     roi_img = []
@@ -126,9 +151,13 @@ def templating(src, temp):
 
     w, h = temp.shape[::-1]
     res = cv2.matchTemplate(src, temp, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.95 # 정확도
-    loc = np.where(res >= threshold)
+    # print(res.shape)
 
+    # print("def templating: ", res)
+
+    threshold = 0.90 # 정확도
+    loc = np.where(res >= threshold)
+    # print("def loc templating: ", loc)
     ary = [] # 좌표값들
     for pt in zip(*loc[::-1]):
         ary2 = [pt]
@@ -138,6 +167,8 @@ def templating(src, temp):
         ary.append(ary2)
         
         cv2.rectangle(src, pt, bottom_right, 0, 1)
+        print("top_left", pt)
+        print("bottom_right", bottom_right)
         # cv2.circle(src, (int((pt[0]*2+w)/2), int((pt[1]*2+h)/2)), 5, 0, 2)
 
     return src, ary
@@ -154,10 +185,12 @@ def search(dirname):
 
     return ary
 
-dirname = "./template"
+dirname = "./template2"
 template_names = search(dirname)
 src = "./images/bears.jpg"
+
 src = imageLoad(src)
+cv2.imshow("src", src)
 src_not = cv2.bitwise_not(src)
 src_not = binaryTo(src_not) # 이진화를 하지않으면 레이블링의 오차범위가 넓어짐
 
@@ -167,22 +200,63 @@ line, fiveline = Findfiveline(binary) # 오선의 좌표값 추출
 
 del_line = delete_line(src, line) # 오선삭제
 del_line = binaryTo(del_line) # 이진화작업
+del_line = cv2.bitwise_not(del_line)
 
 # 모폴로지 연산을 위한 커널 사각형(2 x 2) 생성, 악보크기에 따라 커널 사각형의 값이 적절해야함
-kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+kernal = cv2.getStructuringElement(cv2.MORPH_CROSS, (2, 2))
 
-mophol_img = opening(del_line, kernal) # 모폴로지 연산을 이용해 오선을 제거한 부분을 자연스럽게 매꿈
+# mophol_img = opening(del_line, kernal) # 모폴로지 연산을 이용해 오선을 제거한 부분을 자연스럽게 매꿈
+# mophol_img = closing(del_line, kernal)
+# mophol_img = cv2.erode(del_line, kernal, iterations=1)
+mophol_img = cv2.dilate(del_line, kernal, iterations=1)
+mophol_img = cv2.bitwise_not(mophol_img)
+cv2.imshow("mo", mophol_img)
+
 first_labeling, label_ary = first_labeling(src_not) # 1차 레이블링 레이블한 범위를 배열에 저장
 dst = cv2.bitwise_and(mophol_img, mophol_img, mask = first_labeling) # and연산을 이용해 mophol_img에서 mask부분만 나타냄
-
-# cv2.imshow("first", first_labeling)
-
 first_inv = cv2.bitwise_not(first_labeling) # 배경이미지에 관심영역을 넣기위한 labeling이미지의 inv
 add = cv2.add(dst, first_inv) # 배경과 잘라낸 이미지 합성
+# cv2.imshow("add", add)
+
 add_inv = cv2.bitwise_not(add)
 
 second_labeling2, label_ary2 = second_labeling(add_inv)
+dst2 = cv2.bitwise_and(add, add, mask = second_labeling2)
+second_labeling2_inv = cv2.bitwise_not(second_labeling2)
+add2 = cv2.add(dst2, second_labeling2_inv)
+
+cv2.imshow("add2", add2)
+
+add2_inv = cv2.bitwise_not(add2)
+
+kernal2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+kernal3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+# sobel = cv2.Sobel(add2_inv, -1, 1, 3, ksize=5)
+# mophol_img2 = cv2.morphologyEx(add2_inv, cv2.MORPH_CLOSE, kernal2, iterations=2)
+# mophol_img2 = cv2.morphologyEx(mophol_img2, cv2.MORPH_OPEN, kernal2, iterations=4)
+mophol_img2 = cv2.morphologyEx(add2_inv, cv2.MORPH_DILATE, kernal3, iterations=1)
+mophol_img2 = cv2.morphologyEx(mophol_img2, cv2.MORPH_ERODE, kernal3, iterations=2)
+mophol_img2 = cv2.morphologyEx(mophol_img2, cv2.MORPH_CLOSE, kernal2, iterations=3)
+# mophol_img2 = cv2.erode(add2_inv, kernal2, iterations=1)
+# mophol_img2 = cv2.dilate(mophol_img2, kernal2, iterations=3)
+# mophol_img2 = cv2.erode(mophol_img2, kernal2, iterations=5)
+# mophol_img2 = cv2.dilate(mophol_img2, kernal, iterations=4)
+
+# mophol_img2 = opening(add2_inv, kernal)
+# mophol_img2 = cv2.erode(mophol_img2, kernal, iterations=3)
+# mophol_img2 = cv2.dilate(mophol_img2, kernal, iterations=1)
+# mophol_img2 = cv2.bitwise_not(mophol_img2)
+# cv2.imshow("sobel", sobel)
+cv2.imshow("mo2", mophol_img2)
+
+third = third_labeling(mophol_img2)
+print(fiveline)
+print(third)
+
 second_labeling2 = cv2.bitwise_not(second_labeling2)
+roi_img = make_roi(add, label_ary2)
+
 
 # temp = "./template/high.jpg"
 # temp = imageLoad(temp)
@@ -204,18 +278,18 @@ second_labeling2 = cv2.bitwise_not(second_labeling2)
 # temp4 = binaryTo(temp4)
 # template4, high = templating(add, temp4)
 
-i = 1
-for template_name in template_names:
-    temp = imageLoad(template_name)
-    temp = binaryTo(temp)
-    ary = []
-    cv2.imshow("temp"+str(i), temp)
-    add, ary = templating(add, temp)
-    print("temp"+str(i),":",ary)
+# i = 1
+# for template_name in template_names:
+#     temp = imageLoad(template_name)
+#     temp = binaryTo(temp)
+#     ary = []
+#     cv2.imshow("temp"+str(i), temp)
+#     add, ary = templating(add, temp)
+#     print("temp"+str(i),":",ary)
 
-    i += 1
+#     i += 1
 
-cv2.imshow("add", add)
+# cv2.imshow("add", add)
 
 # cv2.imshow("second_labeling2", second_labeling2)
 # cv2.imshow("temp", template)
@@ -232,13 +306,13 @@ cv2.imshow("add", add)
 # cv2.imshow("mophol_img2", mophol_img2)
 
 # label_ary_inv = [] # 2차레이블링을 위한 레이블링의 inv을 저장하기 위한 배열
-roi_img2 = make_roi(dst, label_ary2) # 레이블 배열을 이용해 영역마다 나눔
+# roi_img2 = make_roi(dst, label_ary2) # 레이블 배열을 이용해 영역마다 나눔
 # template, high = templating(roi_img2, temp)
 # print(high)
 
-for i in range(len(roi_img2)):
+# for i in range(len(roi_img2)):
     # cv2.imshow("roi"+str(i), roi_img2[i])
-    inv = cv2.bitwise_not(roi_img2[i]) # 흑백변환
+    # inv = cv2.bitwise_not(roi_img2[i]) # 흑백변환
     # label_ary_inv.append(inv)
     # cv2.imshow("roi_inv"+str(i), label_ary_inv[i])
 
